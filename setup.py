cc2f5ead120bd4f9f54ef30b66c5076381e004ab
OlukaGibson <gibsonoluka7@gmail.com>
2026-03-30 14:01:44 +0300

    Security: Remove credentials from tracking, refactor to subpackage (v0.0.735)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import pathlib
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
-VERSION = '0.0.734' 
+VERSION = '0.0.735' 
 DESCRIPTION = 'This is used for analyzing the obtain from IoT AirQo devices'
 LONG_DESCRIPTION = README 
 
